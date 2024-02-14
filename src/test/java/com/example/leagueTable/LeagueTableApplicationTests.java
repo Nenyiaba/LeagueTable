6b6bb145b11f7ae4513d25866b24c55dbad3169c
Keex Nenyiaba <keex.nenyiaba@gm.com>
2024-02-14 09:36:15 +0000

    first commit

---
+++ src/test/java/com/example/leagueTable/LeagueTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leagueTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
