ba6e48e5647d6a5d46e353be6ae7efdeccc6826c
Keex Nenyiaba <keex.nenyiaba@gm.com>
2024-02-14 09:40:31 +0000

    another commit

---
+++ src/main/java/com/example/leagueTable/LeagueTableApplication.java
@@ -0,0 +1,13 @@
+package com.example.leagueTable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeagueTableApplication.class, args);
+	}
+
+}
